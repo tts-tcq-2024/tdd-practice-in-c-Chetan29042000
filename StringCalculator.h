e55a22f53ba18901c87697022237a449ec1169a9
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-09-25 11:32:56 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -11,14 +11,14 @@ int is_valid_input(const char* input) {
 // Helper function to sum up numbers from a tokenized string, ignoring numbers > 1000
 int sum_tokens(char* input_copy) {
     int sum = 0;
-    char* token = strtok(input_copy, ",");  // Split the string by commas
+    char* token = strtok(input_copy, ",\n");  // Split the string by commas and newlines
 
     while (token != NULL) {
         int number = atoi(token);  // Convert each token to an integer
         if (number <= 1000) {  // Ignore numbers greater than 1000
             sum += number;
         }
-        token = strtok(NULL, ",");  // Move to the next token
+        token = strtok(NULL, ",\n");  // Move to the next token
     }
     
     return sum;
