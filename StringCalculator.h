6f89a0dd69cc69744bf960d7de46076e63bb3cb2
Chetan29042000 <chetansgowda29042000@gmail.com>
2024-09-25 10:38:24 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -5,13 +5,13 @@
 
 
 // Function to add numbers in a string
-int add(const char* numbers) {
-    // Early return for empty string
-    if (strlen(numbers) == 0 || !isdigit(numbers[0])) {
+int add(const char* input) {
+    // Return for empty string
+    if (strlen(input) == 0 || !isdigit(input[0])) {
         return 0;
     }
     
     // Convert the valid numeric string to an integer and return it
-    return atoi(numbers);
+    return atoi(input);
 }
 
